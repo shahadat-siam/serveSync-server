0b976c8e490baa0167a23ad449e9906983bbe00f
shahadat-siam <lonelinessismycompanion09@gmail.com>
2024-05-10 15:47:01 +0600

    server site initial setup and get data

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+
+//------ middlewere ----
+const corsOptions = {
+    origin:['http://localhost:5173','http://localhost:5174'],
+    credentials:true,
+    optionSuccessStatus:200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+
+ 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ot34xl4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+ 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    const volunteerCollection = client.db('ServeSync').collection('volunteerNeed')
+    
+    
+    // get all data from db
+    app.get('/volunteer', async (req,res) => {
+        const result = await volunteerCollection.find().toArray()
+        res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+     
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('We are going to make an ServeSync.')
+})
+
+app.listen(port, () => {
+    console.log(`ServeSync running on port ${port}`)
+})
