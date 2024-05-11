c1a0cad85399d4c1dfab5e5984add52c0a245125
shahadat-siam <lonelinessismycompanion09@gmail.com>
2024-05-12 00:40:54 +0600

    get specific data by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -44,6 +44,14 @@ async function run() {
       const result = await volunteerCollection.insertOne(volunteerData)
       res.send(result)
   })
+
+  // get specific data from database 
+  app.get('/volunteer/:id', async (req,res) => {
+    const id = req.params.id
+    const query = {_id: new ObjectId(id)}
+    const result = await volunteerCollection.findOne(query)
+    res.send(result)
+  }) 
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
